2eb7ff705e4f41470b3303f8e6b3ead23248d360
JustOff <Off.Just.Off@gmail.com>
2016-11-24 22:10:58 +0200

    ja (initial)

--- ja/browser/defaults/preferences/firefox-l10n.js
+++ ja/browser/defaults/preferences/firefox-l10n.js
@@ -2,4 +2,12 @@
 
 //@line 6 "e:\mozdev\TychoPM\browser\locales\en-US\firefox-l10n.js"
 
-pref("general.useragent.locale", "en-US");
+pref("general.useragent.locale", "ja");
+
+pref("browser.search.order.3", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.4", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.5", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.6", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.7", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.8", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.9", "chrome://browser-region/locale/region.properties");
