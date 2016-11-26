2854a2951cefd8c3f818b5c20491907f748361ee
JustOff <Off.Just.Off@gmail.com>
2016-11-26 17:24:26 +0200

    broken locales cleanup

--- tools/convert.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Converting amp
-for %%a in (*.dtd) do ssr 0 "&amp;" "&" %%a
-echo converting lt and gt
-for %%a in (*.dtd) do ssr 0 "&lt;" "<" %%a
-for %%a in (*.dtd) do ssr 0 "&gt;" ">" %%a
-echo Back-converting " & "
-for %%a in (*.dtd) do ssr 0 " & " " &amp; " %%a
-echo Done!
