9866965e889c7b626f37e601b9c6ca6dd3890d86
JustOff <Off.Just.Off@gmail.com>
2017-05-01 02:43:06 +0300

    Make maxVersion be X.x.* instead of X.*

--- pack.cmd
+++ pack.cmd
@@ -3,6 +3,8 @@ set REL=27.3.0
 rem ar bg da el fi gl hr is kn nl pt-BR pt-PT ro sk sl sr sv-SE tr vi zh-TW
 set LOCALES=cs de en-GB es-AR es-ES es-MX fr hu it ja ko pl ru zh-CN
 
+for /f "tokens=1,2 delims=." %%a in ("%REL%") do set MAX=%%a.%%b
+
 if not "%1" == "" (
   call :pack-xpi %1
   goto :done
@@ -18,7 +20,8 @@ echo Packing %1...
 if exist xpi\%1.xpi del xpi\%1.xpi
 pushd %1
 rem update version
-..\ssr.exe -r -w --unix "em:version\e\q[a-zA-Z0-9_.]+\q"="em:version\e\q%REL%\q" install.rdf
+..\ssr.exe -r -w --unix "em:version\e\q[a-zA-Z0-9_.]+\q"="em:version\e\q%REL%\q" ^
+                        "maxVersion>[a-zA-Z0-9_.\*]+<"="maxVersion>%MAX%.*<"     install.rdf
 rem rename AB-CD -> lang
 move /Y browser\chrome\AB-CD browser\chrome\%1 >nul:
 move /Y chrome\AB-CD\locale\AB-CD chrome\AB-CD\locale\%1 >nul:
