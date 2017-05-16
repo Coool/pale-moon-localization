1c14488c1e5351fda16f990d3a78e2680cf85453
JustOff <Off.Just.Off@gmail.com>
2017-05-16 16:33:52 +0300

    locales update (27.4.0 RC2)

--- sl/browser/defaults/preferences/palemoon-l10n.js
+++ sl/browser/defaults/preferences/palemoon-l10n.js
@@ -1,5 +1,3 @@
-//@line 4 "e:\mozdev\TychoPM\browser\locales\en-US\palemoon-l10n.js"
 
-//@line 6 "e:\mozdev\TychoPM\browser\locales\en-US\palemoon-l10n.js"
 
 pref("general.useragent.locale", "sl");
--- zh-TW/browser/defaults/preferences/palemoon-l10n.js
+++ zh-TW/browser/defaults/preferences/palemoon-l10n.js
@@ -1,6 +1,4 @@
-//@line 4 "e:\mozdev\TychoPM\browser\locales\en-US\palemoon-l10n.js"
 
-//@line 6 "e:\mozdev\TychoPM\browser\locales\en-US\palemoon-l10n.js"
 
 pref("general.useragent.locale", "zh-TW");
 
