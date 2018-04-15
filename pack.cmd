cba634db10b294141727afe221124df38e6af85b
wolfbeast <mcwerewolf@gmail.com>
2018-04-15 22:34:51 +0200

    Update for release 27.9.0

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,5 @@
 @echo off
-set REL=27.8.0
+set REL=27.9.0
 rem ar da fi gl hr id is ja kn ro sk sl sr vi zh-TW
 set LOCALES=bg cs de el en-GB es-AR es-ES es-MX fr hu it ko nl pl pt-BR pt-PT ru sv-SE tr uk zh-CN
 
