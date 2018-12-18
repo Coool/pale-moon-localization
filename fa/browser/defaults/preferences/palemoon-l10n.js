b70e01062f14450c1c265b93dbe2ff322ac4ed93
JustOff <Off.Just.Off@gmail.com>
2018-12-18 18:58:11 +0200

    Add Persian locale

---
+++ fa/browser/defaults/preferences/palemoon-l10n.js
@@ -0,0 +1,5 @@
+//@line 4 "d:\UXP\application\palemoon\locales\en-US\palemoon-l10n.js"
+
+//@line 6 "d:\UXP\application\palemoon\locales\en-US\palemoon-l10n.js"
+
+pref("general.useragent.locale", "fa");
