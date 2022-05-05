eae5c01c8010ded64f8483c7031b41603628c681
Moonchild <moonchild@palemoon.org>
2022-05-05 12:25:22 +0200

    Update source strings for 31.0.0a1

--- en-US/browser/defaults/preferences/palemoon-l10n.js
+++ en-US/browser/defaults/preferences/palemoon-l10n.js
@@ -1,5 +1,5 @@
-//@line 4 "c:\mozdev\palemoon-dev\palemoon\locales\en-US\palemoon-l10n.js"
+//@line 4 "c:\mozdev\PaleMoon\palemoon\locales\en-US\palemoon-l10n.js"
 
-//@line 6 "c:\mozdev\palemoon-dev\palemoon\locales\en-US\palemoon-l10n.js"
+//@line 6 "c:\mozdev\PaleMoon\palemoon\locales\en-US\palemoon-l10n.js"
 
 pref("general.useragent.locale", "en-US");
